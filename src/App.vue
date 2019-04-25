9389248b5f353a5432469a18fe14cbbfde4941d8
sophiafan <172404042@qq.com>
2019-04-25 17:19:53 +0800

    delete old navbar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <div class="company">
-        <img
-          class="logo"
-          src="https://img.freepik.com/free-vector/man-eating-pasta_1087-14.jpg?size=338&ext=jpg"
-          alt
-        >
-        <div class="compname">WeAnalyze</div>
-      </div>
-      <div>
-        <img
-          class="avatar"
-          src="https://img.freepik.com/free-vector/man-eating-pasta_1087-14.jpg?size=338&ext=jpg"
-          alt
-        >
-      </div>
-      <!--  <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>-->
-    </div>
     <div class="scontainer">
       <router-view/>
     </div>
@@ -34,57 +15,14 @@
   text-align: center;
   background-color: #f9fafb;
 }
-#nav {
-  background-color: white;
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-  height: 50px;
-  overflow: hidden;
-  border-bottom: 1px solid #eaeaea;
-
-  height: 70px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 
 body {
   margin: 0px;
   background-color: #f9fafb;
 }
 
-.avatar {
-  border-radius: 50%;
-  width: 50px;
-  border: 1px solid grey;
-}
-
 .scontainer {
   // margin: 50px 80px;
   width: 100%;
-}
-
-.company {
-  display: flex;
-  justify-content: space-between;
-  font-size: 25px;
-  font-weight: bold;
-  position: relative;
-  margin: 0 0 0 15px;
-}
-
-.compname {
-  position: absolute;
-  top: 10px;
-  left: 55px;
-}
-
-.logo {
-  width: 50px;
 }
 </style>
